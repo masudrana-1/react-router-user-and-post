e01b3632c85d5862f3a56b005ecf4c773648820b
masudrana-1 <masudrana58855@gmail.com>
2022-10-05 19:29:18 +0600

    added onclick

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Post.css'
 
 const Post = ({ post }) => {
     const { id, title } = post;
+
+    const navigate = useNavigate();
+
+    const handelNavigate = () => {
+        navigate(`/post/${id}`);
+    }
     return (
         <div className='post'>
             <h1>Every single post is here</h1>
-            <p>ID: <Link to={`/post/${id}`}>{id}</Link></p>
             <h3>Title: {title}</h3>
-        </div>
+
+            {/* 3 ta way te dynamic vabe route declare kora jay */}
+            {/* option 1 */}
+            <Link to={`/post/${id}`}>Visit ID: {id}</Link>
+
+            {/* option 2 */}
+            <Link to={`/post/${id}`}>
+                <button>Show Details</button>
+            </Link>
+
+            {/* option 3 */}
+            <button onClick={handelNavigate}>Show Details 2</button>
+        </div >
     );
 };
 
--- src/components/PostDetails/PostDetails.js
+++ src/components/PostDetails/PostDetails.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const PostDetails = () => {
 
     const post = useLoaderData();
 
     const { id, userId, title, body } = post;
+
+    const navigate = useNavigate();
+
+    const handelNavigate = () => {
+        navigate(`/friend/${userId}`)
+    };
     return (
         <div>
             <h1>Every single post is here......</h1>
@@ -15,6 +21,7 @@ const PostDetails = () => {
             <p>UserId: {userId}</p>
             <h2>Title: {title}</h2>
             <p>Post: {body}</p>
+            <button onClick={handelNavigate}>Get the author</button>
         </div>
     );
 };
