cb2492b668de3201d6735a49017834a2a697b6cb
masudrana-1 <masudrana58855@gmail.com>
2022-10-07 19:29:37 +0600

    add active router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,13 +5,13 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header'>
-            <div>
+            <nav>
                 <NavLink className={({ isActive }) => isActive ? 'active' : undefined} to='/home'>Home</NavLink>
                 <NavLink className={({ isActive }) => isActive ? 'active' : undefined} to='/About'>About</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'active' : undefined} to='/friends'>Friends</NavLink>
                 <NavLink className={({ isActive }) => isActive ? 'active' : undefined} to='/post'>Post</NavLink>
+                <NavLink className={({ isActive }) => isActive ? 'active' : undefined} to='/friends'>Friends</NavLink>
                 <NavLink className={({ isActive }) => isActive ? 'active' : undefined} to='/products'>Products</NavLink>
-            </div>
+            </nav>
             <p>Common header</p>
         </div>
     );
